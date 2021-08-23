4ba7c2eac164201553bbd2c836bab84ccd330e3d
Yoav6 <62355176+Yoav6@users.noreply.github.com>
2021-08-23 12:14:01 +0300

    Update gateway.py

--- bot/modules/gateway.py
+++ bot/modules/gateway.py
@@ -1,11 +1,11 @@
-from . import client
 import re
+from . import client
 
 @client.register_command()
 async def gateway(message, text):
     if message.channel_mentions:
         gateway_to = message.channel_mentions[0]
-        extra_text = " ".join(re.sub('<.*?>', '', text).split())
+        extra_text = ' '.join(re.sub('<.*?>', '', text).split())
         topic_text = f'with topic: {extra_text}' if extra_text else ''
         if message.author.permissions_in(gateway_to).send_messages:
             from_message = await message.channel.send(f'Creating a gateway to {gateway_to.mention}...')
